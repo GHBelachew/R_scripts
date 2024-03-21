35943fa214f1aa7320e8420325fe0f5f95bc998f
GHBelachew <31748273+GHBelachew@users.noreply.github.com>
2024-03-21 09:42:41 -0400

    Update R_ScriptToMigrateDataFrameToSqlTable.r

--- R_ScriptToMigrateDataFrameToSqlTable.r
+++ R_ScriptToMigrateDataFrameToSqlTable.r
@@ -1,9 +1,49 @@
-## This script is to import data frame to SQL Server from r
-## This method is highly important to  do do the employment files, because I couldn't import employment files directly to SQL sever
+## This script is to import dataframe from R to SQL Server
+## This method is highly important to do the employment files, because I couldn't import employment files directly to SQL sever
 
 # Call libraries
 library(DBI)
 library(RODBC)
+library(readxl)
+
+# Clean up the folder by deleting all files
+rm(list = ls()) #  
+
+# R Source site
+#https://www.youtube.com/watch?v=VM-lzkYmyUE
+
+# Define/introduce SQL Server database
+con <- DBI::dbConnect(odbc::odbc(),
+                      driver = "SQL server",
+                      server = "Z4TS01\\SQLEXPRESS",
+                      #database = "Population",
+                      database = "Employment",
+                      Trusted_connection = "True")
+#Read files to Data frame
+df <- read.csv("data.csv")
+#or 
+df <- read_excel("data.xlsx")
+
+# Export the data frame to the SQL
+dbWriteTable(con,"df_DBASEName",df, overrite = TRUE)
+
+# Don't forget to close your connection
+#odbcClose(con)
+on.exit(RODBC::odbcClose(con)) # exit connection
+
+
+#=================================================================
+
+# Examples Below
+
+
+## This script is to import data frame to SQL Server from r
+## This method is highly important to do the employment files, because I couldn't import employment files directly to SQL sever
+
+# Call libraries
+library(DBI)
+library(RODBC)
+library(readxl) # incase he data is from excel
 
 # Clean up the folder by deleting all files
 rm(list = ls()) #  
